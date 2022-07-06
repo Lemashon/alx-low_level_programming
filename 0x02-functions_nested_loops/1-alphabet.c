1a48e579b6ddca1a6dbd9b2ec719cab041e53cd1
Lema05 <lemakipeno@gmail.com>
2022-07-06 18:55:23 +0300

    Function that prints in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
  */
 void print_alphabet(void)
 {
-	char alphabet;
+	char letter;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		_putchar(alphabet);
-        {
+	for (letter = 'a'; letter <= 'z'; letter++)
+        {	
+	    _putchar(letter);
+
 	_putchar('\n');
 }
-
