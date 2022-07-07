e18a83380536779671a94093fe9334f1a8d4256c
Lema05 <lemakipeno@gmail.com>
2022-07-07 21:58:23 +0300

    Get largest number

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+/**
+ * This file contaains a list of all functions used
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
