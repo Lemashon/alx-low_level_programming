b154589a49d35b7691fadd72772e4cfcc62811d8
Lema05 <lemakipeno@gmail.com>
2022-07-09 02:09:26 +0300

    all nested loops push

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,37 +1,13 @@
-/*
- * File: 10-print_triangle.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle, using the character #.
- * @size: The size of the triangle.
+ * print_triangle - use character # to print triangle
+ *
+ * @size: the size of the triangle
+ *
+ * Return: void
  */
 void print_triangle(int size)
 {
-	int hash, index;
-
-	if (size > 0)
-	{
-		for (hash = 1; hash <= size; hash++)
-		{
-			for (index = size - hash; index > 0; index--)
-				_putchar(' ');
-
-			for (index = 0; index < hash; index++)
-				_putchar('#');
-
-			if (hash == size)
-				continue;
-			_putchar('\n');
-		}
-	}
-
-	_putchar('\n');
+	char space;
 }
-
-
-
-
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@
 void print_most_numbers(void)
 {
 	int number;
+
 	for (number = 0; number <= 9; number++)
 	{
 		if (number != 2 && number != 4)
 		{
 			_putchar((number % 10) + '0');
-                }
+		}
 	}
 	_putchar('\n');
 }
